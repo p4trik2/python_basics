8bb78a185d91cad955eb5e3cc5da4f0fac535088
Patrik <kisspatrik214@gmail.com>
2024-10-09 13:51:40 +0200

    classes

---
+++ class.py
@@ -0,0 +1,9 @@
+# user definied data types
+class wardrobe:
+    def __init__(self, color, shelfcount, height):
+        self.color = color
+        self.shelfcount = shelfcount
+        self.height = height
+
+
+wardrobe1 = wardrobe("grey")
